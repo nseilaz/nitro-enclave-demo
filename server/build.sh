bbd38b90b406ccb9926d160f6930f8bf2797b3a8
nseilaz <34375534+nseilaz@users.noreply.github.com>
2021-01-11 15:13:34 +1100

    Update build.sh

--- server/build.sh
+++ server/build.sh
@@ -1,4 +1,4 @@
 docker rmi -f $(docker images -a -q)
-nitro-cli build-enclave --docker-dir ./ --docker-uri enclavetest/nitro-enclave-demo:latest --output-file nitro-enclave-sqs-demo.eif
+nitro-cli build-enclave --docker-dir ./ --docker-uri enclavetest/nitro-enclave-demo:latest --output-file nitro-enclave-demo.eif
 nitro-cli run-enclave --cpu-count 2 --memory 2048 --eif-path nitro-enclave-demo.eif --debug-mode
 nitro-cli console --enclave-id $(nitro-cli describe-enclaves | jq -r ".[0].EnclaveID")
